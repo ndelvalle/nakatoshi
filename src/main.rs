2bc90b1a23b381af5320899dfe2fcf0b0f4656b5
Nicolas Del Valle <nicolas.delvalle@gmail.com>
2020-10-06 01:52:29 -0300

    Add Uncompress CLI flag and iterations count to result (#43)
    * Refactor CLI and add uncompress flag
    
    * Add iterations count to result
    
    * Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,34 @@
-use address::Couple;
+use core::sync::atomic::{AtomicUsize, Ordering};
 use rayon::iter::ParallelIterator;
 use secp256k1::Secp256k1;
-use spinners::{Spinner, Spinners};
-use std::fs::File;
-use std::fs::OpenOptions;
+use std::fs;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::path::PathBuf;
-use std::process;
 use std::time::Instant;
 
 mod address;
 mod cli;
-mod print_result;
+mod output;
 
-use print_result::Output;
+use address::BitcoinAddress;
 
 fn main() {
-    let matches = cli::ask().get_matches();
+    let matches = cli::prompt().get_matches();
 
-    if !matches.is_present("starts-with") && !matches.is_present("file") {
-        eprintln!("Start with prefix must be provided use --starts-with or --file");
-        process::exit(1);
-    }
-
-    let spinner = Spinner::new(Spinners::Dots12, "Finding Bitcoin vanity address\n".into());
     let started_at = Instant::now();
     let secp = Secp256k1::new();
 
-    let is_case_sensitive: bool = matches.is_present("case-sensitive");
-    let is_bech32: bool = matches.is_present("bech32");
+    let is_case_sensitive = matches.is_present("case-sensitive");
+    let is_bech32 = matches.is_present("bech32");
+    let is_compressed = !matches.is_present("uncompressed");
 
-    let num_threads: usize = matches
+    let num_threads = matches
         .value_of("threads")
         .and_then(|duration| duration.parse().ok())
         .unwrap_or_else(num_cpus::get);
 
-    let mut output = Output::new();
+    let mut output = output::Output::new();
 
     // Feature #18 not implemented yet
     let multiple_iterations = false;
@@ -52,12 +44,12 @@ fn main() {
 
     if let Some(output_filename) = matches.value_of("output-file") {
         let file_path = PathBuf::from(output_filename);
-        let output_file = OpenOptions::new()
+        let output_file = fs::OpenOptions::new()
             .write(true)
             .append(true)
             .create_new(true)
             .open(file_path)
-            .expect("Can not open the output file.");
+            .expect("Failed to open output file");
 
         output.add_output_stream(Box::new(output_file));
     }
@@ -67,36 +59,31 @@ fn main() {
         .build()
         .expect("Failed to create thread pool");
 
-    let couple: Couple = rayon_pool.install(|| {
-        rayon::iter::repeat(Couple::new)
-            .map(|couple_new| couple_new(&secp, is_bech32))
-            .find_any(|couple| match matches.value_of("starts-with") {
-                Some(prefix) => couple.starts_with(&prefix, is_case_sensitive),
+    let iterations: AtomicUsize = AtomicUsize::new(0);
+    let bitcoin_address: BitcoinAddress = rayon_pool.install(|| {
+        rayon::iter::repeat(BitcoinAddress::new)
+            .inspect(|_| {
+                iterations.fetch_add(1, Ordering::Relaxed);
+            })
+            .map(|create| create(&secp, is_compressed, is_bech32))
+            .find_any(|bitcoin_address| match matches.value_of("prefix") {
+                Some(prefix) => bitcoin_address.starts_with(&prefix, is_case_sensitive),
                 None => {
-                    let file_name: &str = matches.value_of("file").unwrap();
+                    // TODO: File content should be in memory already
+                    let file_name: &str = matches.value_of("input-file").unwrap();
                     let addresses = get_addresses_from_file(file_name);
 
-                    couple.starts_with_any(&addresses, is_case_sensitive)
+                    bitcoin_address.starts_with_any(&addresses, is_case_sensitive)
                 }
             })
-            .expect("Failed to find address match")
+            .expect("Failed to find Bitcoin address match")
     });
 
-    spinner.stop();
-
-    match matches.value_of("starts-with") {
-        Some(prefix) => output.write(&couple, started_at, prefix, is_case_sensitive),
-        None => {
-            let file_name: &str = matches.value_of("file").unwrap();
-            let addresses = get_addresses_from_file(file_name);
-
-            output.write_from_multiple_addresses(&couple, started_at, addresses, is_case_sensitive);
-        }
-    }
+    output.write(&bitcoin_address, started_at, iterations.into_inner());
 }
 
 fn get_addresses_from_file(file_name: &str) -> Vec<String> {
-    let file = File::open(file_name).unwrap();
+    let file = fs::File::open(file_name).unwrap();
     let buffer = BufReader::new(file);
 
     buffer
@@ -107,26 +94,54 @@ fn get_addresses_from_file(file_name: &str) -> Vec<String> {
 
 #[cfg(test)]
 mod tests {
-    use crate::address::Couple;
+    use crate::address::BitcoinAddress;
     use secp256k1::Secp256k1;
 
     #[test]
-    fn create_bitcoin_public_key() {
+    fn create_compressed_bitcoin_public_key() {
         let secp = Secp256k1::new();
-        let couple = Couple::new(&secp, false);
+        let is_bech32 = false;
+        let is_compressed = true;
+        let bitcoin_address = BitcoinAddress::new(&secp, is_compressed, is_bech32);
 
-        let actual = couple.compressed.public_key.to_string().len();
+        let actual = bitcoin_address.public_key.to_string().len();
         let expected = 66;
 
         assert_eq!(actual, expected);
     }
 
     #[test]
+    fn create_uncompressed_bitcoin_public_key() {
+        let secp = Secp256k1::new();
+        let is_bech32 = false;
+        let is_compressed = false;
+        let bitcoin_address = BitcoinAddress::new(&secp, is_compressed, is_bech32);
+
+        let actual = bitcoin_address.public_key.to_string().len();
+        let expected = 130;
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn create_bech32_address() {
+        let secp = Secp256k1::new();
+        let is_bech32 = true;
+        let is_compressed = true;
+        let bitcoin_address = BitcoinAddress::new(&secp, is_compressed, is_bech32);
+        let address = bitcoin_address.address.to_string();
+
+        assert!(address.starts_with("bc1q"));
+    }
+
+    #[test]
     fn create_bitcoin_private_key() {
         let secp = Secp256k1::new();
-        let couple = Couple::new(&secp, false);
+        let is_bech32 = false;
+        let is_compressed = true;
+        let bitcoin_address = BitcoinAddress::new(&secp, is_compressed, is_bech32);
 
-        let actual = couple.compressed.private_key.to_string().len();
+        let actual = bitcoin_address.private_key.to_string().len();
         let expected = 52;
 
         assert_eq!(actual, expected);
@@ -135,20 +150,13 @@ mod tests {
     #[test]
     fn create_bitcoin_address() {
         let secp = Secp256k1::new();
-        let couple = Couple::new(&secp, false);
+        let is_bech32 = false;
+        let is_compressed = true;
+        let bitcoin_address = BitcoinAddress::new(&secp, is_compressed, is_bech32);
 
-        let actual = couple.compressed.address.to_string().len();
+        let actual = bitcoin_address.address.to_string().len();
         let expected = 34;
 
         assert_eq!(actual, expected);
-    }
-
-    #[test]
-    fn create_bech32_address() {
-        let secp = Secp256k1::new();
-        let couple = Couple::new(&secp, true);
-
-        assert!(couple.uncompressed.address.to_string().starts_with("bc1q"));
-        assert!(couple.compressed.address.to_string().starts_with("bc1q"));
     }
 }
--- src/output.rs
+++ src/output.rs
@@ -31,28 +31,10 @@ impl Output {
 
     pub fn write(
         &mut self,
-        couple: &address::Couple,
+        bitcoin_address: &address::BitcoinAddress,
         started_at: Instant,
-        starts_with: &str,
-        is_case_sensitive: bool,
+        iterations: usize,
     ) {
-        let is_compressed = couple
-            .compressed
-            .starts_with(starts_with, is_case_sensitive);
-        let (private_key, public_key, address) = if is_compressed {
-            (
-                couple.compressed.private_key.to_string(),
-                couple.compressed.public_key.to_string(),
-                couple.compressed.address.to_string(),
-            )
-        } else {
-            (
-                couple.uncompressed.private_key.to_string(),
-                couple.uncompressed.public_key.to_string(),
-                couple.uncompressed.address.to_string(),
-            )
-        };
-
         if let Some(log_stream) = &mut self.log_stream {
             let duration = Instant::now() - self.last_print_time;
             let millis = duration.as_millis();
@@ -71,36 +53,18 @@ impl Output {
         }
 
         let result = json!({
-            "private_key": private_key,
-            "public_key": public_key,
-            "address": address,
-            "compressed": is_compressed,
-            "creation_time": started_at.elapsed()
+            "private_key": bitcoin_address.private_key.to_string(),
+            "public_key": bitcoin_address.public_key.to_string(),
+            "address": bitcoin_address.address.to_string(),
+            "seconds": started_at.elapsed().as_secs(),
+            "iterations": iterations
         });
 
         for stream in self.output_streams.iter_mut() {
             let result_string = result.to_string() + "\n";
-            let _written = stream.write_all(result_string.as_bytes());
-        }
-    }
-
-    pub fn write_from_multiple_addresses(
-        &mut self,
-        couple: &address::Couple,
-        started_at: Instant,
-        addresses: Vec<String>,
-        is_case_sensitive: bool,
-    ) {
-        for address in addresses {
-            if couple
-                .compressed
-                .starts_with(address.as_str(), is_case_sensitive)
-                || couple
-                    .uncompressed
-                    .starts_with(address.as_str(), is_case_sensitive)
-            {
-                self.write(couple, started_at, address.as_str(), is_case_sensitive);
-            }
+            stream
+                .write_all(result_string.as_bytes())
+                .expect("Failed to write Bitcoin address result");
         }
     }
 }
