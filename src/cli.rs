dffeb0566b75a192441608a88cd85972ad6fc0c6
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-01-17 16:22:28 -0300

    Upgrade clap CLI (#124)

--- src/cli.rs
+++ src/cli.rs
@@ -1,25 +1,25 @@
-pub fn prompt<'a, 'b>() -> clap::App<'a, 'b> {
+pub fn prompt<'a>() -> clap::App<'a> {
     clap::App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
-            clap::Arg::with_name("prefix")
+            clap::Arg::new("prefix")
                 .index(1)
                 .takes_value(true)
-                .required_unless_one(&["input-file"])
+                .required_unless_present_any(&["input-file"])
                 .help("Prefix used to match addresses"),
         )
         .arg(
-            clap::Arg::with_name("input-file")
-                .short("i")
+            clap::Arg::new("input-file")
+                .short('i')
                 .long("input-file")
                 .takes_value(true)
-                .required_unless_one(&["prefix"])
+                .required_unless_present_any(&["prefix"])
                 .help("File with prefixes to match addresses with"),
         )
         .arg(
-            clap::Arg::with_name("threads")
-                .short("t")
+            clap::Arg::new("threads")
+                .short('t')
                 .long("threads")
                 .takes_value(true)
                 // This is not the actual default value, it is here to pretty display
@@ -28,23 +28,23 @@ pub fn prompt<'a, 'b>() -> clap::App<'a, 'b> {
                 .help("Number of threads to be used"),
         )
         .arg(
-            clap::Arg::with_name("case-sensitive")
-                .short("c")
+            clap::Arg::new("case-sensitive")
+                .short('c')
                 .long("case-sensitive")
                 .takes_value(false)
                 .help("Use case sensitive comparison to match addresses"),
         )
         .arg(
-            clap::Arg::with_name("bech32")
+            clap::Arg::new("bech32")
                 .conflicts_with("case-sensitive")
-                .short("b")
+                .short('b')
                 .long("bech32")
                 .takes_value(false)
                 .help("Use Bech32 addresses. Starting with bc1q (Lowercase address)"),
         )
         .arg(
-            clap::Arg::with_name("uncompressed")
-                .short("u")
+            clap::Arg::new("uncompressed")
+                .short('u')
                 .long("uncompressed")
                 .takes_value(false)
                 .help("Use uncompressed private an public keys"),
