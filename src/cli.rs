c4e40ddba2d125ed0661fcd8d5c436bd3cf1a718
Nicolas del Valle <nicolas.delvalle@gmail.com>
2022-11-03 23:27:59 +0100

    Update dependecies (#234)
    * Update dependecies
    
    * Remove not used lifetime anotation
    
    * Update code style based on cargo clippy

--- src/cli.rs
+++ src/cli.rs
@@ -1,27 +1,24 @@
-pub fn prompt<'a>() -> clap::Command<'a> {
+pub fn prompt() -> clap::Command {
     clap::Command::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             clap::Arg::new("prefix")
                 .index(1)
-                .takes_value(true)
-                .required_unless_present_any(&["input-file"])
+                .required_unless_present_any(["input-file"])
                 .help("Prefix used to match addresses"),
         )
         .arg(
             clap::Arg::new("input-file")
                 .short('i')
                 .long("input-file")
-                .takes_value(true)
-                .required_unless_present_any(&["prefix"])
+                .required_unless_present_any(["prefix"])
                 .help("File with prefixes to match addresses with"),
         )
         .arg(
             clap::Arg::new("threads")
                 .short('t')
                 .long("threads")
-                .takes_value(true)
                 // This is not the actual default value, it is here to pretty display
                 // the information.
                 .default_value("The number of CPUs available on the current system")
@@ -31,7 +28,7 @@ pub fn prompt<'a>() -> clap::Command<'a> {
             clap::Arg::new("case-sensitive")
                 .short('c')
                 .long("case-sensitive")
-                .takes_value(false)
+                .action(clap::ArgAction::SetTrue)
                 .help("Use case sensitive comparison to match addresses"),
         )
         .arg(
@@ -39,14 +36,14 @@ pub fn prompt<'a>() -> clap::Command<'a> {
                 .conflicts_with("case-sensitive")
                 .short('b')
                 .long("bech32")
-                .takes_value(false)
+                .action(clap::ArgAction::SetTrue)
                 .help("Use Bech32 addresses. Starting with bc1q (Lowercase address)"),
         )
         .arg(
             clap::Arg::new("uncompressed")
                 .short('u')
                 .long("uncompressed")
-                .takes_value(false)
+                .action(clap::ArgAction::SetTrue)
                 .help("Use uncompressed private an public keys"),
         )
 }
